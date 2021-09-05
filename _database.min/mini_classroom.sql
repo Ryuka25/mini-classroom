b458b1dc554b02c4ff1afc560ab65aac9caa8bbf
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-05 07:27:16 +0300

    📦️ NEW `mini_classroom.sql` : Create the database

---
+++ _database.min/mini_classroom.sql
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------------------- #
+# Project name:          Mini_Classroom                                  #
+# Author:                @Lovanirina (ryuka25.github.io)                 #
+# ---------------------------------------------------------------------- #
+
+DROP DATABASE IF EXISTS mini_classroom;
+
+CREATE DATABASE IF NOT EXISTS mini_classroom;
+
+USE mini_classroom;
