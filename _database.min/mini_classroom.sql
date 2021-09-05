30ebf0d095f242c409bb10523663898f1dc863e3
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-05 07:30:26 +0300

    ♻️ UPDATE `mini_classroom.sql` : Add table "Modules"

--- _database.min/mini_classroom.sql
+++ _database.min/mini_classroom.sql
@@ -8,3 +8,26 @@ DROP DATABASE IF EXISTS mini_classroom;
 CREATE DATABASE IF NOT EXISTS mini_classroom;
 
 USE mini_classroom;
+
+# ---------------------------------------------------------------------- #
+# Tables                                                                 #
+# ---------------------------------------------------------------------- #
+# ---------------------------------------------------------------------- #
+# Add table "Modules"                                                    #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `Modules` (
+    `moduleId` VARCHAR(25) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `description` MEDIUMTEXT,
+    `picture` LONGBLOB NOT NULL,
+    `categoryCode` VARCHAR(25) NOT NULL,
+    CONSTRAINT `PK_modules` PRIMARY KEY (`moduleId`),
+);
+
+# ---------------------------------------------------------------------- #
+# Foreign key constraints                                                #
+# ---------------------------------------------------------------------- #
+
+ALTER TABLE `Modules` ADD CONSTRAINT `FK_modules_moduleCategories`
+    FOREIGN KEY (`moduleCategoryCode`) REFERENCES `ModuleCategories`(`moduleCategoryCode`);
