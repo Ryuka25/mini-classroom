4fc226e79494e3cc9fe24c0801021e2ee93a7212
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-08 21:39:31 +0300

    📦️ ADD `controller/Posts.php`

---
+++ controller/Posts.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Posts {
+
+    private $postID;
+    private $title;
+    private $publicationDateTime;
+    private $category;
+    private $deadline;
+    private $legend;
+    private $moduleID;
+    private $accountID;
+
+}
