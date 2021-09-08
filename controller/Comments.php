5fabd0651e81d87b927133fdb1d3b2e37b00e80d
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-08 21:38:21 +0300

    📦️ ADD `controller/Comments.php`

---
+++ controller/Comments.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Comments {
+
+    private $commentID;
+    private $creationDateTime;
+    private $legend;
+    private $attachedFile;
+    private $accountID;
+    private $postID;
+
+}
