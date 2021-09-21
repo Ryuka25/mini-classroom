7aff36e347ba1b97a14c8882d0c5a732f71abc19
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-21 18:30:50 +0300

    📦️ ADD `models/Model.php`

---
+++ models/Message.php
@@ -0,0 +1,30 @@
+<?php 
+include_once('Model.php');
+
+class Message extends Model {
+
+    private $attachedDiscussion;
+    private $attachedFile;
+    private $legend;
+    private $messageID;
+    private $sendByStudent;
+    private $sendDateTime;
+
+    
+    /**
+    * SETTERS for all keys
+    */
+    public function set($key, $value) {
+        if (property_exists($this, $key)) {
+            $this->$key = $value;
+        }
+    }
+
+
+    /**
+    * GETTERS for all keys
+    */
+    public function get($key) {
+        return $this->$key;
+    }
+}
