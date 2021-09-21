a5dc178c2e16a0a0493cdb877edcece27c84ff4b
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-21 18:42:11 +0300

    📦️ ADD `models/Account.php`

---
+++ models/Account.php
@@ -0,0 +1,35 @@
+<?php 
+include('Model.php');
+
+class Account extends Model {
+
+    private $accountID;
+    private $address;
+    private $adminAccess;
+    private $associedSchoolClass;
+    private $firstName;
+    private $lastName;
+    private $password;
+    private $phoneNumber;
+    private $picture;
+    private $type;
+
+    
+    /**
+    * SETTERS for all keys
+    */
+    public function set($key, $value) {
+        if (property_exists($this, $key)) {
+            $this->$key = $value;
+        }
+    }
+
+
+    /**
+    * GETTERS for all keys
+    */
+    public function get($key) {
+        return $this->$key;
+    }
+
+}
