7943d7307b46dc4e6a57bb4bf537edc0ad5632ce
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-21 18:08:07 +0300

    📦️ ADD `models/Schedule.php`

---
+++ models/Shedule.php
@@ -0,0 +1,33 @@
+<?php 
+include('Model.php');
+
+class Shedule extends Model {
+
+    private $sheduleID;
+    private $sheduleDate;
+    private $startTime;
+    private $finishTime;
+    private $schoolRoom;
+    private $accountID;
+    private $classLevel;
+    private $moduleID;
+
+    
+    /**
+    * SETTERS for all keys
+    */
+    public function set($key, $value) {
+        if (property_exists($this, $key)) {
+            $this->$key = $value;
+        }
+    }
+
+
+    /**
+    * GETTERS for all keys
+    */
+    public function get($key) {
+        return $this->$key;
+    }
+    
+}
