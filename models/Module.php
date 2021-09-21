79acb5eb249bf397f1ea189cdad50d7cfcc74ded
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-21 17:58:02 +0300

    🐛️ FIX `models/Module.php`

--- models/Module.php
+++ models/Module.php
@@ -1,4 +1,5 @@
 <?php
+include_once('Model.php');
 
 class Module extends Model {
 
@@ -8,4 +9,21 @@ class Module extends Model {
     private $picture;
     private $moduleCategoryCode;
 
+
+    /**
+    * SETTERS for all keys
+    */
+    public function set($key, $value) {
+        if (property_exists($this, $key)) {
+            $this->$key = $value;
+        }
+    }
+
+
+    /**
+    * GETTERS for all keys
+    */
+    public function get($key) {
+        return $this->$key;
+    }
 }
