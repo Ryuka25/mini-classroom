4c0ce8b8c89eb4d101c73930e23ffd36c8662a32
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-21 17:42:08 +0300

    ♻️ UPDATE `models/Module.php` : Delete Static Getters & Setters

--- models/Module.php
+++ models/Module.php
@@ -1,62 +1,11 @@
 <?php
 
-class Module {
+class Module extends Model {
 
     private $moduleID;
     private $name;
     private $description;
     private $picture;
     private $moduleCategoryCode;
-
-
-    /***********/
-    /* SETTERS */
-    /***********/
-
-    
-    public function setModuleID($newModuleID) {
-        $this->moduleID = $newModuleID;
-    }
-
-    public function setName($newName) {
-        $this->name = $newName;
-    }
-    
-    public function setDescription($newDescription) {
-        $this->description = $newDescription;
-    }
-
-    public function setPicture($newPicture) {
-        $this->picture = $newPicture;
-    }
-
-    public function setModuleCategoryCode($newModuleCategoryCode) {
-        $this->moduleCategoryCode = $newModuleCategoryCode;
-    }    
-
-
-    /***********/
-    /* GETTERS */
-    /***********/
-
-    public function getModuleID() {
-        return $this->moduleID;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function getDescription() {
-        return $this->description;
-    }
-
-    public function getPicture() {
-        return $this->picture;
-    }
-
-    public function getModuleCategoryCode() {
-        return $this->moduleCategoryCode;
-    }
 
 }
