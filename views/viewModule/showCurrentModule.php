8d1a9ffcd484108db0b51be0ae6547525e753b5d
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-30 05:32:35 +0300

    📦️ ADD `views` | `viewModule`

---
+++ views/viewModule/showCurrentModule.php
@@ -0,0 +1,9 @@
+<tr>
+    <td class="text-center" scope="row"><?= $module->getModuleId()?></td>
+    <td><?= $module->getName() ?></td>
+    <td><?= $module->getDescription() ?></td>
+    <td class="text-center">
+        <a class="btn btn-warning" href="#" role="button">Update</a> | 
+        <a class="btn btn-danger" href="#" role="button">Delete</a>
+    </td>
+</tr>
