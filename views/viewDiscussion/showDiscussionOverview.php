df26253d137d4267dccea1ec253c9228f4b27261
Ryuka25 <lovanirina.ran@gmail.com>
2021-09-30 05:24:24 +0300

    📦️ ADD `views` | `viewDiscussion`

---
+++ views/viewDiscussion/showDiscussionOverview.php
@@ -0,0 +1,5 @@
+<div>
+    <a class="btn btn-secondary mt-2" href="<?= SERVER_URL.'?url=discussion/'.$discussion->getDiscussionID().'/message/' ?>">
+        <?= $discussion->getName(); ?>
+    </a>
+</div>
