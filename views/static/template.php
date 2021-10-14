7c8af455845417718b7221b6b494ae77e5c018f5
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-14 11:31:22 +0300

    📦️ ADD `fontawesome` to `template`

--- views/static/template.php
+++ views/static/template.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="http://localhost/web-toolkit/bootstrap/4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/web-toolkit/fontawesome/free/5.15.3-web/css/all.css"/>
+
     <link rel="stylesheet" href="<?= SERVER_URL.'/views/static/style/style.css'?>">
     <title><?= $pageTitle ?> - Mini-classroom</title>
 </head>
@@ -17,7 +19,7 @@
     
 
     <!-- Boostrap JS-->
-    <script src="http://localhost/web-toolkit/bootstrap/4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="http://localhost/web-toolkit/bootstrap/4.6.0/dist/js/bootstrap.bundle.js"></script>
     
 </body>
 </html>
