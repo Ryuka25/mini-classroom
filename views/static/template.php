55e465d9ffd8737cdd25bde2cb9fc56b007d9986
Ryuka25 <lovanirina.ran@gmail.com>
2021-10-24 07:49:47 +0300

    📦️ ADD : 'background' to 'login-page'

--- views/static/template.php
+++ views/static/template.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="<?= SERVER_URL.'/views/static/style/style.css'?>">
     <title><?= $pageTitle ?> - Mini-classroom</title>
 </head>
-<body class="bg-dark">
+<body>
 
     <?= $topNavBar ?>
 
--- views/viewLogin/login.php
+++ views/viewLogin/login.php
@@ -1,33 +1,39 @@
 <!-- Begin Login Page -->
-<div class="col-xl-4 float-right d-flex">
-    <div class="card col-12 vh-100 rounded-0">
-        <div class="card-body">
-            <div class="row justify-content-center">
-                <div class="col-12">
-                    <div class="text-center">
-                        <h1 class="h4 mb-4">Login now!</h1>
-                    </div>
-                    <form class="user small" method="POST" action="<?= SERVER_URL.'/?url=login/'?>">
-                        <div class="form-group">
-                            <input type="text" class="form-control form-control-user"
-                                id="accountID" name="accountID" 
-                                placeholder="Enter Username ..." value="<?= $accountID ?>">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control form-control-user"
-                                name="password" 
-                                placeholder="Password">
-                            <div class="text-danger text-center" id="passwordError">
-                                <p class="loginError"><?=$error?></p>
+<div class="login-page">
+    <div class="row">
+        <div class="col-xl-8 float-left login-hero">
+        </div>
+        <div class="col-xl-4 float-right d-flex">
+            <div class="card col-12 vh-100 rounded-0">
+                <div class="card-body">
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="text-center">
+                                <h1 class="h4 mb-4">Login now!</h1>
                             </div>
+                            <form class="user small" method="POST" action="<?= SERVER_URL.'/?url=login/'?>">
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user"
+                                        id="accountID" name="accountID" 
+                                        placeholder="Enter Username ..." value="<?= $accountID ?>">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-user"
+                                        name="password" 
+                                        placeholder="Password">
+                                    <div class="text-danger text-center" id="passwordError">
+                                        <p class="loginError"><?=$error?></p>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-user btn-block"> Login </button>
+                                </div>
+                            </form>
+                            <hr>
+                            <div class="text-center"><a href="#createAccount" class="small">Create an Account!</a></div>
+                            <div class="text-center"><a href="<?= SERVER_URL."/?url=home/"?>" class="small">Login as Guest</a></div>
                         </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-user btn-block"> Login </button>
-                        </div>
-                    </form>
-                    <hr>
-                    <div class="text-center"><a href="#createAccount" class="small">Create an Account!</a></div>
-                    <div class="text-center"><a href="<?= SERVER_URL."/?url=home/"?>" class="small">Login as Guest</a></div>
+                    </div>
                 </div>
             </div>
         </div>
